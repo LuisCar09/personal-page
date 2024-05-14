3c55751395d6f5a21f3dc4d484d630d6c4b96f26
Luis Carlos Bravo <luiscbravo94@gmail.com>
2024-05-14 16:12:35 +0100

    Agregado menu lateral

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,12 @@
 
 //Hacemos click en el menu, nos muestra y nos oculta la lista en el menu
 $('.navbar-menu').on('click',function () {
-    $('.navbar-list--toogle').fadeToggle(400,'linear')
+    // $('.navbar-list--toogle').fadeIn(500,'linear')
+    $('.navbar-list--toogle').addClass('moveLeft')
+    console.log('click')
+})
+
+$('.close-navbar-second').on('click', ()=>{
+    // $('.navbar-list--toogle').fadeOut(500,'linear')
+    $('.navbar-list--toogle').removeClass('moveLeft')
 })
