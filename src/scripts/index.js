c3a1fa5698df4f996cad5b415cbf287325beeda8
Luis Carlos <luiscbravo94@gmail.com>
2025-03-18 17:25:38 +0100

    added repo fetching

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 
+const gitHubUser = 'LuisCar09'
 //Hacemos click en el menu, nos muestra y nos oculta la lista en el menu
 $('.navbar-menu').on('click',function () {
     // $('.navbar-list--toogle').fadeIn(500,'linear')
@@ -10,3 +11,18 @@ $('.close-navbar-second').on('click', ()=>{
     // $('.navbar-list--toogle').fadeOut(500,'linear')
     $('.navbar-list--toogle').removeClass('moveLeft')
 })
+
+
+const getProjects = async () => {
+    const response = await fetch(`https://api.github.com/users/${gitHubUser}/repos`)
+    const data = await response.json()
+    console.log(data);
+    
+    // const deployedRepositories = data.map(dat => console.log(dat.homepage))
+    // console.log(deployedRepositories);
+    
+
+}
+
+
+window.addEventListener('load',getProjects)
